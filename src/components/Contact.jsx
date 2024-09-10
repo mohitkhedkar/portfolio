64ca16df41fb3fabd7196c65169ca743ac8307fa
Mohit Khedkar <mohitkhedkar521@gmail.com>
2024-09-11 04:57:33 +0530

    framer motion added to portfolio

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,13 +2,28 @@ import React from "react";
 import { CONTACT } from "../constants";
 import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
 import { FaX } from "react-icons/fa6";
+import { motion } from "framer-motion";
 
 function Contact() {
   return (
     <div className="border-t border-stone-900 pb-20">
-      <h2 className="my-10 text-center text-4xl">Contact Details</h2>
+      <motion.h2
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 0.5 }}
+        className="my-10 text-center text-4xl"
+      >
+        Contact Details
+      </motion.h2>
       <div className="text-center tracking-wider ">
-        <p className="my-4">{CONTACT.phoneNo}</p>
+        <motion.p
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: -100 }}
+          transition={{ duration: 1 }}
+          className="my-4"
+        >
+          {CONTACT.phoneNo}
+        </motion.p>
         <a href="">{CONTACT.email}</a>
       </div>
 
--- src/components/IntroSection.jsx
+++ src/components/IntroSection.jsx
@@ -1,6 +1,30 @@
 import React from "react";
 import profilePic from "../assets/profilePic.png";
 import { HERO_CONTENT } from "../constants";
+import { motion } from "framer-motion";
+
+const containerVariants = {
+  hidden: { opacity: 0, x: -100 },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 0.5,
+      staggerChildren: 0.5,
+    },
+  },
+};
+
+const childVariants = {
+  hidden: { opacity: 0, x: -100 },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 0.5,
+    },
+  },
+};
 
 function IntroSection() {
   return (
@@ -11,26 +35,46 @@ function IntroSection() {
       >
         <div className="w-full lg:w-1/2">
           <div className="flex justify-center lg:p-8">
-            <img
+            <motion.img
               src={profilePic}
               alt="Mohit Khedkar"
               className="border border-stone-900 rounded-3xl"
+              width={650}
+              height={650}
+              initial={{ x: 100, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 1, delay: 1.5 }}
             />
           </div>
         </div>
         <div className="w-full lg:w-1/2">
-          <div className="flex flex-col items-center lg:items-start mt-10">
-            <h2 className="pb-2 text-4xl tracking-tighter lg:text-8xl">
+          <motion.div
+            initial="hidden"
+            animate="visible"
+            variants={containerVariants}
+            className="flex flex-col items-center lg:items-start mt-10"
+          >
+            <motion.h2
+              variants={childVariants}
+              className="pb-2 text-4xl tracking-tighter lg:text-8xl"
+            >
               Mohit Khedkar
-            </h2>
-            <span className="bg-gradient-to-r from-stone-300 to-stone-500 bg-clip-text text-3xl tracking-tight text-transparent">
+            </motion.h2>
+            <motion.span
+              variants={childVariants}
+              className="bg-gradient-to-r from-stone-300 to-stone-500 bg-clip-text text-3xl tracking-tight text-transparent"
+            >
               {" "}
               Frontend Developer
-            </span>
-            <p className="my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter text-center">
+            </motion.span>
+            <motion.p
+              variants={childVariants}
+              className="my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter text-center"
+            >
               {HERO_CONTENT}
-            </p>
-            <a
+            </motion.p>
+            <motion.a
+              variants={childVariants}
               href="/resume.pdf"
               target="_blank"
               rel="noopener noreferrer"
@@ -38,8 +82,8 @@ function IntroSection() {
               className="bg-white rounded-full p-4 text-sm text-stone-800 mb-10"
             >
               Download Resume
-            </a>
-          </div>
+            </motion.a>
+          </motion.div>
         </div>
       </div>
     </div>
