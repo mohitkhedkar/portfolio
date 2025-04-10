f433622ae386efa170f2684025cc53f0ab7e4cf6
Mohit Khedkar <mohitkhedkar521@gmail.com>
2025-04-11 04:09:58 +0530

    added vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Technologies from "./components/Technologies";
 import Projects from "./components/Projects";
 import Experience from "./components/Experience";
 import Contact from "./components/Contact";
+import { Analytics } from "@vercel/analytics/next";
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
         {/* <Experience /> */}
         <Contact />
       </div>
+      <Analytics />
     </>
   );
 }
